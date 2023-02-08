a630b276ac037885f0f0277d29919e2ac95ae4c6
KingofHarmony <andy_noel@hotmail.com>
2023-02-08 13:05:48 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿///Andy Mateo
+///CPS 3330 section 3 Lab2
+namespace Lab2
+{
+    public class Lab2
+    {
+        public static void averageNum(int a, int b)
+        {
+            Console.WriteLine("Provided integers: {0:N4} and {1:N4}, the average is {2:N5}", a, b, Convert.ToDouble(a + b) / 2);
+        }
+        public static void averageNum(int a, int b, int? c)
+        {
+            Console.WriteLine("Provided integers: {0:N4} and {1:N4} and {2:N4}, the average is {3:N5}", a, b, c, Convert.ToDouble(a + b + c) / 3);
+        }
+    }
+}
